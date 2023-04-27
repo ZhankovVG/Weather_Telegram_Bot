e36dc5ebd054e2be6c18593ec8a73a4c7dc708c1
Vitaliy <sprinter-100m@ukr.net>
2023-04-27 20:45:26 +0300

    Weather Bot

---
+++ config.py
@@ -0,0 +1,4 @@
+import telebot
+
+bot = telebot.TeleBot('5951886439:AAGatqMJR0dGXOAFuloW2BSGsZd0PE-zTuc')
+weather_token = '2070be680625e8d2ff928eda62414d95'
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import telebot
 import requests
 import json
+import datetime
+from config import weather_token, bot
+import telebot
+
 
 bot = telebot.TeleBot('5951886439:AAGatqMJR0dGXOAFuloW2BSGsZd0PE-zTuc')
 weather_token = '2070be680625e8d2ff928eda62414d95'
@@ -8,21 +11,39 @@ weather_token = '2070be680625e8d2ff928eda62414d95'
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, 'Привет')
+    bot.send_message(message.chat.id, 'Привет! Напиши мне название города и я пришлю сводку погоды')
 
 
 @bot.message_handler(content_types=['text'])
 def get_weather(message):
     city = message.text.strip().lower()
-    res = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={weather_token}&units=metric')
+    res = requests.get(
+        f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={weather_token}&units=metric')
     if res.status_code == 200:
         data = json.loads(res.text)
-        temp = data['main']['temp']
 
-
+        city = data['name']
+        temperature = data['main']['temp']
         
-        bot.reply_to(message, f'Сейчас погода: {temp}')
+        humidity = data['main']['humidity']
+        pressure = data['main']['pressure']
+        wind = data['main']['temp']
+        sunries_timestamp = datetime.datetime.fromtimestamp(
+            data['sys']['sunrise'])
+        sunset_timestamp = datetime.datetime.fromtimestamp(
+            data['sys']['sunset'])
+        lenght_of_the_day = datetime.datetime.fromtimestamp(data['sys']['sunset']) - datetime.datetime.fromtimestamp(
+            data['sys']['sunrise'])
+        
+        
 
+        bot.reply_to(message,
+                     f'***{datetime.datetime.now().strftime("%Y-%m-%d %H:%M")}***\n'
+                     f'Погода в городе: {city}\nТемпература: {temperature} C°\n'
+                     f'Влажность: {humidity} %\nДавление: {pressure} мм.рт.ст\nВетер: {wind} м.с\n'
+                     f'Восход солнца: {sunries_timestamp}\nЗакат солнца: {sunset_timestamp}\nПродолжительность дня: {lenght_of_the_day}\n'
+                     f'***Хорошего вам дня!***'
+                     )
 
     else:
         bot.reply_to(message, f'Город указан не верно')
