8eaa73df33f48fd94a14da2bf551fcef719ae9b5
Vitaliy <sprinter-100m@ukr.net>
2023-04-28 19:16:04 +0300

    Weather Bot

--- main.py
+++ main.py
@@ -6,9 +6,6 @@ import telebot
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
-bot = telebot.TeleBot('5951886439:AAGatqMJR0dGXOAFuloW2BSGsZd0PE-zTuc')
-weather_token = '2070be680625e8d2ff928eda62414d95'
-
 
 @bot.message_handler(commands=['start'])
 def start(message):
